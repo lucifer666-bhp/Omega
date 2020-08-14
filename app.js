2e9591843b9b01d5c1400102c91f8b20dd79ddf5
lucifer666-bhp <69681447+lucifer666-bhp@users.noreply.github.com>
2020-08-14 19:05:10 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyA5y9iNd-ax08i3PwZMFvWGs9UcW_VgcoY",
+  authDomain: "fbook-6f0c3.firebaseapp.com",
+  databaseURL: "https://fbook-6f0c3.firebaseio.com",
+  projectId: "fbook-6f0c3",
+  storageBucket: "fbook-6f0c3.appspot.com",
+  messagingSenderId: "830632094563",
+  appId: "1:830632094563:web:d62d66b5d8830de59ac156",
+  measurementId: "G-R7FHCYGMNW"
 };
-
      
  
 ////Do Not Edit Below This
